b068a9de902897bfbdc49ad545542307e46b3637
lalo <eduardoarandah@gmail.com>
2018-03-29 18:55:18 -0600

    init

---
+++ README.md
@@ -0,0 +1,47 @@
+# Descarga Masiva CFDI Firefox
+
+Descarga masivamente los CFDI (XML y PDF) del SAT México
+
+![descargarxml](https://user-images.githubusercontent.com/4065733/38107780-1d87b470-3350-11e8-9faf-ef1413bf79a9.png)
+
+# Cómo Instalar
+
+### 1) Descarga la extensión en ZIP
+
+### 2) Descomprime la carpeta en donde gustes
+![descomprimir](https://user-images.githubusercontent.com/4065733/38109729-58fbedd6-3356-11e8-845f-3e89237d15fd.png)
+
+### 3) En Firefox ve a la dirección "about:debugging#addons"
+
+
+### 4) Haz click en "Cargar complemento temporal" y selecciona el archivo manifest.json
+
+
+### 5) Deberá aparecer un icono en el menú
+![instalada](https://user-images.githubusercontent.com/4065733/38109730-59472efe-3356-11e8-87f6-ad41ab932848.png)
+
+# Cómo usar
+
+### 1) Visita el sitio del SAT [https://portalcfdi.facturaelectronica.sat.gob.mx](https://portalcfdi.facturaelectronica.sat.gob.mx)
+### 2) Ve a **Consultar Facturas Recibidas**
+### 3) Haz click en el icono que aparece en su navegador y listo
+
+![icono](https://user-images.githubusercontent.com/4065733/38109726-5899f7de-3356-11e8-8fed-04dfda6c5c77.png)
+
+# Para analizar los CFDI
+
+Puedes usar la herramienta [Analizador CFDI](https://github.com/eduardoarandah/AnalizadorCFDI)
+
+# Desarrollo
+
+Contáctame si necesitas ayuda: 
+
+eduardoarandah@gmail.com
+
+**Si deseas contribuir al desarrollo**
+
+Puedes hacerlo via paypal en el siguiente enlace
+[https://eduardoarandah.github.io/](https://eduardoarandah.github.io/)
+
+![donar](https://user-images.githubusercontent.com/4065733/38109725-587af320-3356-11e8-941a-7215489a9286.png)
+
---
+++ popup.js
@@ -0,0 +1,62 @@
+var allLinks = [];
+//listener que recibe los enlaces de send_links.js
+browser.runtime.onMessage.addListener(function(links) {
+    console.log("ejecutando linksEncontrados");
+    allLinks = links;
+    //cambiamos los textos de los botones
+    document.getElementById('descargarxml').innerText = "Descargar " + allLinks[0].length + " XML";
+    document.getElementById('descargarpdf').innerText = "Descargar " + allLinks[1].length + " PDF";
+});
+
+function descargarxml() {
+    //los xml están en el array en posición 0
+    document.getElementById('status').textContent = "Descargando XML...";
+    console.log(allLinks);
+    console.log(allLinks[0]);
+    console.log(allLinks[2]);
+    for (var i = 0; i < allLinks[0].length; i++) {
+        //https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/downloads        
+        var liga = allLinks[0][i];
+        var name = allLinks[2][i]+".xml";
+        console.log("descargando url:" + liga + " nombre: " + name);
+        browser.downloads.download({
+            url: liga,
+            saveAs:false,
+            filename:name
+        });
+    }
+}
+
+function descargarpdf() {
+    //los xml están en el array en posición 1
+    document.getElementById('status').textContent = "Descargando PDF...";
+    for (var i = 0; i < allLinks[1].length; i++) {
+        //https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/downloads
+        var liga = allLinks[1][i];
+        var name = allLinks[2][i]+".pdf";
+        console.log("descargando url:" + liga + " nombre: " + name);
+        browser.downloads.download({
+            url: liga,
+            saveAs:false,
+            filename:name
+        });
+    }
+}
+window.onload = function() {
+    document.getElementById('descargarxml').onclick = descargarxml;
+    document.getElementById('descargarpdf').onclick = descargarpdf;
+    document.getElementById('analizar').onclick = function() {
+        browser.tabs.create({
+            url: 'https://analizador-cfdi.netlify.com/'
+        });
+    }
+    document.getElementById('enlace').onclick = function() {
+        browser.tabs.create({
+            url: 'https://github.com/eduardoarandah/AnalizadorCFDI'
+        });
+    }
+    //esta función agrega un JS a la tab activa para enviar los enlaces al listener
+    browser.tabs.executeScript({
+        file: "/send_links.js"
+    });
+};
