2fbb51d51eb849e3535d2eea8d9f7923efc1d4d3
lalo <eduardoarandah@gmail.com>
2018-08-31 11:20:10 -0500

    mejoras visuales

--- README.md
+++ README.md
@@ -1,43 +1,62 @@
-# Descarga Masiva CFDI para Firefox
+# Descarga Masiva CFDI para Google Chrome
 
-Descarga masivamente los CFDI (XML y PDF) del SAT México. 
+Descarga masivamente los CFDI (XML y PDF) del [SAT México](https://sat.gob.mx/) 
 
-Esta aplicación es gratuita y open source. 
+![portada](https://user-images.githubusercontent.com/4065733/44919085-36755880-ad02-11e8-97a0-c6620ab20bf8.png)
 
-Contáctame si deseas integrar con tus sistemas empresariales. 
+# Instalación
 
-![descargarxml](https://user-images.githubusercontent.com/4065733/38107780-1d87b470-3350-11e8-9faf-ef1413bf79a9.png)
+### Instala la extensión **gratis** desde Firefox Add-ons
 
-# Cómo Instalar
+[https://addons.mozilla.org/en-US/firefox/addon/descarga-masiva-facturas/](https://addons.mozilla.org/en-US/firefox/addon/descarga-masiva-facturas/)
 
-### 1) Necesitas tener instalado [Firefox](https://www.mozilla.org/es-MX/firefox/new/)
+# Cómo Funciona
 
-### 2) Instala el plugin aquí: [https://addons.mozilla.org/en-US/firefox/addon/descarga-masiva-facturas/](https://addons.mozilla.org/en-US/firefox/addon/descarga-masiva-facturas/)
-
-### 3) Deberá aparecer un icono en el menú
-![icono](https://user-images.githubusercontent.com/4065733/38120044-3aa6e368-3383-11e8-9067-511cc730a453.png)
-
-# Cómo usar
-
-### 1) Visita el sitio del SAT [https://portalcfdi.facturaelectronica.sat.gob.mx](https://portalcfdi.facturaelectronica.sat.gob.mx)
-### 2) Ve a **Consultar Facturas Recibidas**
-### 3) Haz click en el icono que aparece en su navegador y listo
+### 1) Click en el icono
 
 ![icono](https://user-images.githubusercontent.com/4065733/38109726-5899f7de-3356-11e8-8fed-04dfda6c5c77.png)
 
-### 4) Después analiza tus XML en: [https://analizador-cfdi.netlify.com](https://analizador-cfdi.netlify.com)
+### 2) Ir al SAT
+
+![manual1](https://user-images.githubusercontent.com/4065733/44919090-3a08df80-ad02-11e8-9f19-8af0c53f569b.png)
+
+### 3) Puedes descargar emitidas o recibidas
+
+![manual2](https://user-images.githubusercontent.com/4065733/44919091-3aa17600-ad02-11e8-8727-13d0fc40784a.png)
+
+### 4) Selecciona el periodo
+
+![manual3](https://user-images.githubusercontent.com/4065733/44919092-3aa17600-ad02-11e8-8331-4b9706f96021.png)
+
+### 5) Click en el icono
+
+![manual4](https://user-images.githubusercontent.com/4065733/44919093-3b3a0c80-ad02-11e8-8f3c-561765b571d3.png)
+
+### 6) (OPCIONALMENTE) Puedes analizar las facturas en mi analizador
+
+[https://github.com/eduardoarandah/AnalizadorCFDI](https://github.com/eduardoarandah/AnalizadorCFDI)
+
+![manual5](https://user-images.githubusercontent.com/4065733/44919094-3b3a0c80-ad02-11e8-9b58-179629287198.png)
+
+![manual6](https://user-images.githubusercontent.com/4065733/44919095-3bd2a300-ad02-11e8-9003-30cadc4edf71.png)
 
 
-# Desarrollo
 
-Contáctame si necesitas ayuda: 
 
-eduardoarandah@gmail.com
+# Programación de sistemas
 
-**Si deseas contribuir al desarrollo**
+Para proyectos de programación escribe a: eduardoarandah@gmail.com
 
-Puedes hacerlo via paypal en el siguiente enlace
+# Si deseas contribuir
+
+**He invertido mi tiempo realizado este proyecto con la intención de ayudar a todos.** 
+
+**Si te ha hecho la vida más fácil, puedes contribuir con una donación**
+
+Ve a esta página y haz click en "donar" 
+
 [https://eduardoarandah.github.io/](https://eduardoarandah.github.io/)
 
-![donar](https://user-images.githubusercontent.com/4065733/38109725-587af320-3356-11e8-941a-7215489a9286.png)
+![Eduardo Aranda Hernández](https://user-images.githubusercontent.com/4065733/38109725-587af320-3356-11e8-941a-7215489a9286.png)
 
+*Ing. Eduardo Aranda Hernández*
--- popup.js
+++ popup.js
@@ -1,12 +1,4 @@
 var allLinks = [];
-//listener que recibe los enlaces de send_links.js
-browser.runtime.onMessage.addListener(function(links) {
-    // console.log("ejecutando linksEncontrados");
-    allLinks = links;
-    //cambiamos los textos de los botones
-    document.getElementById('descargarxml').innerText = "Descargar " + allLinks[0].length + " XML";
-    document.getElementById('descargarpdf').innerText = "Descargar " + allLinks[1].length + " PDF";
-});
 
 function descargarxml() {
     //los xml están en el array en posición 0
@@ -17,12 +9,12 @@ function descargarxml() {
     for (var i = 0; i < allLinks[0].length; i++) {
         //https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/downloads        
         var liga = allLinks[0][i];
-        var name = allLinks[2][i]+".xml";
+        var name = allLinks[2][i] + ".xml";
         // console.log("descargando url:" + liga + " nombre: " + name);
         browser.downloads.download({
             url: liga,
-            saveAs:false,
-            filename:name
+            saveAs: false,
+            filename: name
         });
     }
 }
@@ -33,30 +25,62 @@ function descargarpdf() {
     for (var i = 0; i < allLinks[1].length; i++) {
         //https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/downloads
         var liga = allLinks[1][i];
-        var name = allLinks[2][i]+".pdf";
+        var name = allLinks[2][i] + ".pdf";
         // console.log("descargando url:" + liga + " nombre: " + name);
         browser.downloads.download({
             url: liga,
-            saveAs:false,
-            filename:name
+            saveAs: false,
+            filename: name
         });
     }
 }
+//listener que recibe los enlaces de send_links.js
+browser.runtime.onMessage.addListener(function(links) {
+    // console.log("ejecutando linksEncontrados");
+    allLinks = links;
+    //cambiamos los textos de los botones
+    document.getElementById('cuenta-xml').innerText = allLinks[0].length;
+    document.getElementById('cuenta-pdf').innerText = allLinks[1].length;
+});
 window.onload = function() {
+    //botones
     document.getElementById('descargarxml').onclick = descargarxml;
     document.getElementById('descargarpdf').onclick = descargarpdf;
+    //enlaces
     document.getElementById('analizar').onclick = function() {
         browser.tabs.create({
             url: 'https://analizador-cfdi.netlify.com/'
         });
     }
-    document.getElementById('enlace').onclick = function() {
+    document.getElementById('iralsat').onclick = function() {
         browser.tabs.create({
-            url: 'https://github.com/eduardoarandah'
+            url: 'https://portalcfdi.facturaelectronica.sat.gob.mx'
         });
     }
-    //esta función agrega un JS a la tab activa para enviar los enlaces al listener
-    browser.tabs.executeScript({
-        file: "/send_links.js"
+    document.getElementById('enlace').onclick = function() {
+        browser.tabs.create({
+            url: 'https://eduardoarandah.github.io/'
+        });
+    }
+    document.getElementById('manual').onclick = function() {
+        browser.tabs.create({
+            url: 'https://github.com/eduardoarandah/DescargaMasivaCFDIFirefox'
+        });
+    }
+    //esta función inyecta un JS a la tab activa para enviar los enlaces al listener
+    browser.windows.getCurrent(function(currentWindow) {
+        browser.tabs.query({
+            active: true,
+            windowId: currentWindow.id
+        }, function(activeTabs) {
+            //checar si url contiene "https://portalcfdi.facturaelectronica.sat.gob.mx/"
+            var url = activeTabs[0].url;
+            var estamos_en_sat = url.startsWith("https://portalcfdi.facturaelectronica.sat.gob.mx");
+            if (estamos_en_sat) {
+                browser.tabs.executeScript(activeTabs[0].id, {
+                    file: 'inject.js'
+                });
+            }
+        });
     });
 };
